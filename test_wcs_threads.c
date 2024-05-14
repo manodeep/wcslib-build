c37a111949ec07200089b330c300c304bf076fd6
Manodeep Sinha <manodeep@gmail.com>
2024-05-14 21:42:35 +1000

    Replaced my version with Mark's

--- test_wcs_threads.c
+++ test_wcs_threads.c
@@ -12,7 +12,7 @@ typedef int (*roundtrip_coords_impls)(const int N, double *pixcrd, double *imgcr
 
 int set_wcs(struct wcsprm *wcs)
 {
-    //This replicates the wcs setup in astropy issue #16245
+    //This replicates the wcs setup in astropy issue 16245
     assert(wcs != NULL);
     assert(wcs->naxis == 2);
     wcs->crpix[0] = -234.75;
@@ -23,11 +23,7 @@ int set_wcs(struct wcsprm *wcs)
     wcs->crval[1] = -90.0;
     sprintf(wcs->ctype[0], "RA---AIR");
     sprintf(wcs->ctype[1], "DEC--AIR");
-#ifdef USE_FLAG_TO_BYPASS
-#warning "Using the flag to bypass the wcsset() call in wcslib-8.3 onwards"
     wcs->flag = 1;
-    fprintf(stderr,"Setting wcs->flag = 1 to bypass the wcsset() call in wcslib-8.3 onwards\n");
-#endif
     return wcsset(wcs);
 }
 
@@ -94,6 +90,7 @@ int roundtrip_coords_buggy(const int N, double *pixcrd, double *imgcrd, double *
     fprintf(stderr,"Using %s with nthreads = %d\n", __FUNCTION__, nthreads);
     #pragma omp parallel num_threads(nthreads) shared(wcs, pixcrd, imgcrd, phi, theta, world, stat, dest)
     {
+        assert(omp_get_num_threads() == nthreads);
         const int tid = omp_get_thread_num();
         int start, n_thistask;
         distribute_compute_over_ntasks(N, nthreads, tid, &n_thistask, &start);
@@ -110,17 +107,16 @@ int roundtrip_coords_buggy(const int N, double *pixcrd, double *imgcrd, double *
         fprintf(stderr,"On thread %d: start = %d, end = %d n_thistask = %d\n", tid, start, end, n_thistask);
         int status = wcsp2s(wcs, n_thistask, naxis, local_pixcrd, local_imgcrd, local_phi, local_theta, local_world, local_stat);
         if(status != EXIT_SUCCESS) {
-            fprintf(stderr, "[on tid=%d]: wcsp2s failed with status %d\n", tid, status);
+            fprintf(stderr, "wcsp2s failed with status %d\n", status);
         }
 
-        //calling wcsset() within a parallel region causes the race condition
-        fprintf(stderr,"[On tid = %d] wcs->flag = %d\n", tid, wcs->flag);
+        //calling wcsset() causes the race condition (needs nowait to trigger it)
         wcsset(wcs);
 
         //convert back to pixel coordinates
         status = wcss2p(wcs, n_thistask, naxis, local_world, local_phi, local_theta, local_imgcrd, local_pixcrd, local_stat);
         if(status != EXIT_SUCCESS) {
-            fprintf(stderr, "[on tid=%d]:wcss2p failed with status %d\n", tid, status);
+            fprintf(stderr, "wcss2p failed with status %d\n", status);
         }
 
         memcpy(local_dest, local_pixcrd, 2*n_thistask*sizeof(*local_dest));
@@ -130,12 +126,15 @@ int roundtrip_coords_buggy(const int N, double *pixcrd, double *imgcrd, double *
 }
 
 
+
+
 int roundtrip_coords_omp_barrier(const int N, double *pixcrd, double *imgcrd, double *phi, double *theta, double *world, int *stat, double *dest, struct wcsprm *wcs, const int nthreads)
 {
     const int naxis = wcs->naxis;
     fprintf(stderr,"Using %s with nthreads = %d\n", __FUNCTION__, nthreads);
     #pragma omp parallel num_threads(nthreads) shared(wcs, pixcrd, imgcrd, phi, theta, world, stat, dest)
     {
+        assert(omp_get_num_threads() == nthreads);
         const int tid = omp_get_thread_num();
         int start, n_thistask;
         distribute_compute_over_ntasks(N, nthreads, tid, &n_thistask, &start);
@@ -153,10 +152,10 @@ int roundtrip_coords_omp_barrier(const int N, double *pixcrd, double *imgcrd, do
 
         int status = wcsp2s(wcs, n_thistask, naxis, local_pixcrd, local_imgcrd, local_phi, local_theta, local_world, local_stat);
         if(status != EXIT_SUCCESS) {
-            fprintf(stderr, "[on tid=%d]:wcsp2s failed with status %d\n", tid, status);
+            fprintf(stderr, "wcsp2s failed with status %d\n", status);
         }
 
-        //calling wcsset() causes the race condition but can be prevented with the barrier and single
+        //calling wcsset() causes the race condition (needs nowait to trigger it)
         #pragma omp barrier
         #pragma omp single
         wcsset(wcs);
@@ -165,7 +164,7 @@ int roundtrip_coords_omp_barrier(const int N, double *pixcrd, double *imgcrd, do
         //convert back to pixel coordinates
         status = wcss2p(wcs, n_thistask, naxis, local_world, local_phi, local_theta, local_imgcrd, local_pixcrd, local_stat);
         if(status != EXIT_SUCCESS) {
-            fprintf(stderr, "[on tid=%d]:wcss2p failed with status %d\n", tid, status);
+            fprintf(stderr, "wcss2p failed with status %d\n", status);
         }
         memcpy(local_dest, local_pixcrd, 2*n_thistask*sizeof(*local_dest));
     }
@@ -223,12 +222,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "Memory allocation failed for N = %d\n", N);
         return EXIT_FAILURE;
     }
-    // const roundtrip_coords_impls all_functions[] = { &roundtrip_coords_omp_barrier, &roundtrip_coords_buggy};
-    // const char *function_names[72] = { "roundtrip_coords_omp_barrier", "roundtrip_coords_buggy"};
-    // const int n_functions = sizeof(all_functions)/sizeof(all_functions[0]);
-
-    const roundtrip_coords_impls all_functions[] = { &roundtrip_coords_buggy};
-    const char *function_names[72] = { "roundtrip_coords_buggy"};
+    const roundtrip_coords_impls all_functions[] = { &roundtrip_coords_omp_barrier, &roundtrip_coords_buggy};
+    const char *function_names[72] = { "roundtrip_coords_omp_barrier", "roundtrip_coords_buggy"};
     const int n_functions = sizeof(all_functions)/sizeof(all_functions[0]);
 
     const double min = -1000.0;
@@ -236,7 +231,6 @@ int main(int argc, char **argv)
     setup_random_pixcrd(N, min, max, pixcrd);
     memcpy(pixcrd_orig, pixcrd, 2*N*sizeof(*pixcrd));
 
-    int64_t num_wrong = 0;
     for(int nthreads=1;nthreads<=max_numthreads;nthreads++) {
         for(int ifunc=0;ifunc<n_functions;ifunc++) {
             struct timespec t0, t1;
@@ -249,7 +243,6 @@ int main(int argc, char **argv)
             clock_gettime(CLOCK_MONOTONIC, &t1);
             const double func_time = (t1.tv_sec - t0.tv_sec) + 1e-9*(t1.tv_nsec - t0.tv_nsec);
             const int n_mismatches = count_n_mismatches(N, pixcrd_orig, dest);
-            num_wrong += n_mismatches;
             fprintf(stderr,"Called function = '%s' with nthreads=%d, N=%d, n_mismatches = %d. Time taken = %g seconds\n",
                          function_names[ifunc], nthreads, N,  n_mismatches, func_time);
             memcpy(pixcrd, pixcrd_orig, 2*N*sizeof(*pixcrd));
@@ -266,5 +259,67 @@ int main(int argc, char **argv)
     free(stat);
     free(pixcrd_orig);
 
-    return num_wrong;
+    return EXIT_SUCCESS;
 }
+//I created the file in the cextern/wcslib/C directory and compiled with:
+//clang -g -I.. -Wall -Wextra -fsanitize=leak -fsanitize=undefined -fsanitize=bounds -fsanitize=address -fsanitize-undefined-trap-on-error -fstack-protector-all test_wcs_threads.c -std=c99 -O3 -Wl,-no_compact_unwind -fopenmp=libomp libwcs-PIC.a -o test_wcs_threads
+
+//Running on a single thread with 1 million points produces same results after roundtrip (tol = 1e-8):
+/*
+On thread 0: start = 0, end = 10000000 n_thistask = 10000000
+Called function = 'roundtrip_coords_omp_barrier' with nthreads=1, N=10000000, n_mismatches = 0. Time taken = 2.9328 seconds
+On thread 0: start = 0, end = 10000000 n_thistask = 10000000
+Called function = 'roundtrip_coords_buggy' with nthreads=1, N=10000000, n_mismatches = 0. Time taken = 2.76603 seconds
+*/
+
+//Running on multiple threads sometimes produces a crash with double-free, and sometimes the n_mismatches > 0:
+/*
+(.venv) [~/sorsery-consulting/2024/reproject-astropy-wcs/astropy/cextern/wcslib/C @HAW10013056] ./test_wcs_threads 100000 2
+On thread 0: start = 0, end = 100000 n_thistask = 100000
+Called function = 'roundtrip_coords_omp_barrier' with nthreads=1, N=100000, n_mismatches = 0. Time taken = 0.029945 seconds
+On thread 0: start = 0, end = 100000 n_thistask = 100000
+Called function = 'roundtrip_coords_buggy' with nthreads=1, N=100000, n_mismatches = 0. Time taken = 0.028093 seconds
+On thread 0: start = 0, end = 50000 n_thistask = 50000
+On thread 1: start = 50000, end = 100000 n_thistask = 50000
+Called function = 'roundtrip_coords_omp_barrier' with nthreads=2, N=100000, n_mismatches = 95872. Time taken = 0.016187 seconds
+On thread 0: start = 0, end = 50000 n_thistask = 50000
+On thread 1: start = 50000, end = 100000 n_thistask = 50000
+=================================================================
+==67903==ERROR: AddressSanitizer: attempting double-free on 0x0001034001b0 in thread T0:
+    #0 0x100ce19f0 in wrap_free+0x8c (libclang_rt.asan_osx_dynamic.dylib:arm64+0x3d9f0) (BuildId: e7b5fb9640823e93be90c001748b8fc332000000200000000100000000000b00)
+    #1 0x10055822c in linset lin.c:747
+    #2 0x100574074 in wcsset.part.0 wcs.c:2807
+    #3 0x10054bc70 in .omp_outlined. test_wcs_threads.c:89
+    #4 0x10081daf8 in __kmp_invoke_microtask+0x98 (libomp.dylib:arm64+0x81af8) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+
+0x0001034001b0 is located 0 bytes inside of 16-byte region [0x0001034001b0,0x0001034001c0)
+freed by thread T1 here:
+    #0 0x100ce19f0 in wrap_free+0x8c (libclang_rt.asan_osx_dynamic.dylib:arm64+0x3d9f0) (BuildId: e7b5fb9640823e93be90c001748b8fc332000000200000000100000000000b00)
+    #1 0x10055822c in linset lin.c:747
+    #2 0x100574074 in wcsset.part.0 wcs.c:2807
+    #3 0x10054bc70 in .omp_outlined. test_wcs_threads.c:89
+    #4 0x10081daf8 in __kmp_invoke_microtask+0x98 (libomp.dylib:arm64+0x81af8) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+
+previously allocated by thread T0 here:
+    #0 0x100ce1c68 in wrap_calloc+0x90 (libclang_rt.asan_osx_dynamic.dylib:arm64+0x3dc68) (BuildId: e7b5fb9640823e93be90c001748b8fc332000000200000000100000000000b00)
+    #1 0x100558238 in linset lin.c:748
+    #2 0x100574074 in wcsset.part.0 wcs.c:2807
+    #3 0x10054c830 in .omp_outlined..13 test_wcs_threads.c:131
+    #4 0x10081daf8 in __kmp_invoke_microtask+0x98 (libomp.dylib:arm64+0x81af8) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+
+Thread T1 created by T0 here:
+    #0 0x100cdbc10 in wrap_pthread_create+0x50 (libclang_rt.asan_osx_dynamic.dylib:arm64+0x37c10) (BuildId: e7b5fb9640823e93be90c001748b8fc332000000200000000100000000000b00)
+    #1 0x1007fff10 in __kmp_create_worker+0xcc (libomp.dylib:arm64+0x63f10) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+    #2 0x1007be35c in __kmp_allocate_thread+0x410 (libomp.dylib:arm64+0x2235c) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+    #3 0x1007b8f34 in __kmp_allocate_team+0x96c (libomp.dylib:arm64+0x1cf34) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+    #4 0x1007ba980 in __kmp_fork_call+0x13b4 (libomp.dylib:arm64+0x1e980) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+    #5 0x1007ade9c in __kmpc_fork_call+0xa8 (libomp.dylib:arm64+0x11e9c) (BuildId: 583abbdbdb43371e8f4bf9100abf532b32000000200000000100000000000c00)
+    #6 0x10054c220 in roundtrip_coords_omp_barrier test_wcs_threads.c:131
+    #7 0x10054d354 in main test_wcs_threads.c:233
+    #8 0x1008b9088 in start+0x204 (dyld:arm64+0x5088) (BuildId: 75627683a78032adae34cf86dd23a26b32000000200000000100000000050c00)
+    #9 0xf12efffffffffffc  (<unknown module>)
+
+SUMMARY: AddressSanitizer: double-free (libclang_rt.asan_osx_dynamic.dylib:arm64+0x3d9f0) (BuildId: e7b5fb9640823e93be90c001748b8fc332000000200000000100000000000b00) in wrap_free+0x8c
+==67903==ABORTING
+Abort trap: 6
+*/
